6435bf1fc2ac66ee0d597bc4dc46e869cc9bcfff
shlomitha <shlomit.hadad1@gmail.com>
2016-11-01 21:29:31 +0200

    Solution for: sol-html5-web-development.javascript-syntax-lab/solutions.js

--- 13-javascript-syntax-lab/solutions.js
+++ 13-javascript-syntax-lab/solutions.js
@@ -1,33 +1,66 @@
-/**
- * Starter file for 13-javascript-syntax-lab.
- * Fill in the blanks in each function
- */
+//function 1
+printBiggestNumber();
 
-/**
- * Write a function that randomizes 3 numbers in range [0,100],
- * prints all 3 and the largest one
- */
-function ex1() {
+//function 2
+printAmountOfNumber();
+
+//function 3
+printGCD(9,6);
+
+//function 4
+ printLCM(9,6);
+
+
+function printBiggestNumber(){	
+	var max=100;
+	var min=0;	
+	var first_num=Math.floor((Math.random() * max) + min);
+	var secound_num=Math.floor((Math.random() * max) + min);
+	var third_num=Math.floor((Math.random() * max) + min);
+	  
+	console.log("first_num ="+ first_num);	
+	console.log("secound_num ="+ secound_num);
+	console.log("third_num ="+ third_num);
+	
+	var max_number=Math.max(first_num, secound_num,third_num);	
+	console.log("max_number ="+ max_number);	
+	
+};
+
+function printAmountOfNumber (){	
+	var max=1000;
+	var min=9999;	
+	var num=Math.floor((Math.random() * max) + min);
+	num=""+num;
+	var len=num.length;
+    console.log("number ="+ num);  
+    var amount=0;   
+    for(var i=0;i<len;i++) {
+    	amount+=Number(num.charAt(i));
+    }
+	console.log("amount ="+ amount);
 }
 
-/**
- * Write a function that randomizes a number in range [1000, 9999]
- * and prints the sum total of its digits
- */
-function ex2() {
-}
 
-/**
- * Write a function that takes two numbers as arguments
- * and prints their gcd
- */
-function gcd(a, b) {
-}
+//Least Common Multiple 
+function printLCM(x,y) {
+var lcm= (x*y) / calcGCD(x,y);
+console.log('LCM number='+ lcm); 
+}  
 
-/**
- * Write a function that takes two numbers as arguments
- * and prints their lcm
- */
-function lcm(a, b) {
+//Greatest Common Divisor
+function printGCD(x, y) {   
+  console.log('GCD number='+ calcGCD(x,y));  
+}  
+
+function calcGCD(x,y){
+  x = Math.abs(x);  
+  y = Math.abs(y);  
+  while(y) {  
+    var t = y;  
+    y = x % y;  
+    x = t;
+    } 
+    return x;  	
 }
 
