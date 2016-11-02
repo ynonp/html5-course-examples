e2a26d28cf8601f445b4e0a03b5c5111e2d4bef6
shlomitha <shlomit.hadad1@gmail.com>
2016-11-02 03:30:49 -0700

    Solution for: sol-html5-web-development.arrays-objects-lab/solutions.js

--- 17-arrays-objects-lab/solutions.js
+++ 17-arrays-objects-lab/solutions.js
@@ -1,56 +1,30 @@
-/**
- * Starter file for lesson:
- * arrays-objects-lab
- *
- * Fill in code in the functions body
- */
+sum();
+sum(2, 3, 5);
 
 /**
- * Write a function that prints the sum total of its arguments
- */
-function sum() {
+- * Write a function that get parameters
+- * and prints their amount
+- */
+function sum(){   
+ var arr= [].slice.call(arguments);  
+  if (arr.length===0 )
+     console.log(0);
+  else{
+     console.log(arr.reduce((a,b)=>a+b));   
+  }  
 }
 
+uniq('foo', 'foo', 'foo', 'bar', 'bar', 'buz', 'buz');
+uniq(10, 10, 10, 20, 20, 10, 20, 30);
+
 /**
- * Write a function that takes a list of words and return the
- * list without duplicate values
- */
-function uniq() {
+- * Write a function that get parameters
+- * and prints all parameters without duplicate 
+- */
+function uniq(){  
+  var duplicatesArray= [].slice.call(arguments);  
+  var uniqueArray = duplicatesArray.filter(function(elem, pos) {
+  return duplicatesArray.indexOf(elem) == pos;
+});
+ console.log(uniqueArray);
 }
-
-/**
- * Example usage: Open the html file to see results in your browser
- *
- */
-
-describe('#sum', function() {
-  it('should sum positive numbers', function() {
-    expect(sum(10, 20, 30)).toEqual(60);
-  });
-
-	it('should sum any number of arguments', function() {
-		expect(sum(1,2,3,4,5,6,7,8,9,10)).toEqual(55);
-	});
-
-  it('should sum negative numbers', function() {
-    expect(sum(-10, -20, -30)).toEqual(-60);
-  });
-
-  it('should return 0 if no arguments passed', function() {
-    expect(sum()).toEqual(0);
-  });
-});
-
-
-describe('#uniq', function() {
-	it('should return uniq values', function() {
-    expect(uniq('foo', 'foo', 'foo', 'bar', 'bar', 'buz', 'buz')).
-      toEqual(['foo', 'bar', 'buz']);
-	});
-
-  it('should work for numbers too', function() {
-    expect(uniq(10, 10, 10, 20, 20, 10, 20, 30)).
-      toEqual([10, 20, 30]);
-  });
-});
-
