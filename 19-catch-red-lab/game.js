b9116478f2d855d1e0973b539e48d1eb438b1108
shlomitha <shlomit.hadad1@gmail.com>
2016-11-12 11:45:24 -0800

    Solution for: 19-catch-red-lab/game.js

--- 19-catch-red-lab/game.js
+++ 19-catch-red-lab/game.js
@@ -1,26 +1,37 @@
 var game=document.querySelector('.game');
 var scoreTxt=document.querySelector('.score');
 var allEl=document.querySelectorAll('.game>div');
+var redSquare=document.querySelector('.winner');
 var redIndex=0,score=0;
 game.addEventListener('click',chatchTheRed);
+var timeout=setTimeout(function(){ mixSquares(0); }, 2000);
 
 function chatchTheRed(ev){	
-	var redSquare=document.querySelector('.winner');	
+	var scoreToAdd=0;
 	if(!ev.target.nodeName=="DIV")
 	return;
 	
 	if (ev.target.classList.contains("winner")){
-		score+=5;		
+		scoreToAdd=5;		
 	}
 	else{
-		score-=5;
+		scoreToAdd=-5;
 	}
+	clearTimeout(timeout);
+	mixSquares(scoreToAdd);
+}
+
+function mixSquares(scoreToAdd){	
+	score+=scoreToAdd;	
 	scoreTxt.innerText='Score: '+ score;
+	redSquare=document.querySelector('.winner');	
 	redSquare.classList.remove("winner");
-	redIndex=randIndex(0, 4, redIndex);	
+	redIndex=randIndex(0, allEl.length, redIndex);	
 	allEl[Number(redIndex)].classList.add("winner");
+	timeout=setTimeout(function(){ mixSquares(-5); }, 2000);
 	
 }
+
 function randIndex(lowest, highest, excluded) {
 	var result=0;
 	while (result==excluded){
